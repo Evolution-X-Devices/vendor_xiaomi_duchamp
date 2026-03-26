4eff30bcb3e2d5ee9938f28d35237def5e69b8c0
MT6897A <tomilindanya@mail.ru>
2026-03-26 19:49:02 +0000

    duchamp: Update proprietarity and firmware from OS.2.0.205.0.VNLMIXM

---
+++ Android.mk
@@ -0,0 +1,32 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),duchamp)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,fb7462cf6806b2dc87d306f9e9f635b92ada652f)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,4951e9687392ef2e9a8339fc1d85bcd1bbe97823)
+$(call add-radio-file-sha1-checked,radio/ccu.img,377c1275beedd967be55e4aa8b0ad6e03d0b70b0)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,3de04b78d2b43c590ba3ab85160da26b843ad8db)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,b24cf2d818d5749c173cc45ef5c024e40dfb021d)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,3d6a2e05de1568b8fd53521910c77987363b8b51)
+$(call add-radio-file-sha1-checked,radio/dpm.img,84a96147850c99d0776fdf18ed742347e108c30b)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,7d6af74e2e108156aa487f75ce615e7260f845da)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,f4ff6b4845538228a26620cb8d4be3f76eb54621)
+$(call add-radio-file-sha1-checked,radio/gz.img,127098b46bbd1d16db770dbe2ec735bdd0b371db)
+$(call add-radio-file-sha1-checked,radio/lk.img,a5a4734d0f2f8891d9028aca5fa261271b9f0e67)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,8926b26619baf843c500fdbf3f0380bd25e3e0b4)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,5b553462a61ec9dc8dd39bbd3a42fb9342c6e68f)
+$(call add-radio-file-sha1-checked,radio/modem.img,8adcdeccf86d5616baf6afd42f5ba805698ca7bf)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,4637da6b3fd0e766d6a74c2a419d0ec8af7b174f)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,655fa420459c663e639a000e64485e9cd31c12d5)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,26088e2318f922611e0e8c7fd016f631d9dc8ae3)
+$(call add-radio-file-sha1-checked,radio/scp.img,3da3858a9482cfbad3f90152365dcb17804b001b)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,920a994c1e6344acf471de885ee8457854d8947f)
+$(call add-radio-file-sha1-checked,radio/sspm.img,5cbbd327d3297d37b5930e288ce4637b9174dd96)
+$(call add-radio-file-sha1-checked,radio/tee.img,fc57dee503302369bc9483d42ce97142bb4d64c2)
+$(call add-radio-file-sha1-checked,radio/vcp.img,2ab27f87613e16a8a8eb59033b7d178955b76b4c)
+
+endif
